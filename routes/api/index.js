4a53a26745ffca7d37f5c3a414a58df7c75cf12f
Pavel Darii <paveldarii@yahoo.com>
2021-03-14 17:58:35 -0400

    added actionRoutes to the use method

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,9 +2,10 @@ const router = require("express").Router();
 const userRoutes = require("./users");
 const projectRoutes = require("./projects");
 const tasksRoutes = require("./tasks");
+const actionsRoutes = require("./actions");
 
 router.use("/users", userRoutes);
 router.use("/projects", projectRoutes);
 router.use("/tasks", tasksRoutes);
-
+router.use("/action", actionRoutes);
 module.exports = router;
