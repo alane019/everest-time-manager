8c201cff3cd1eb12cfdf7841f166ccbc22b165d7
Pavel Darii <paveldarii@yahoo.com>
2021-03-14 17:13:22 -0400

    inserted all the routes in the router.use method

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,10 @@
 const router = require("express").Router();
 const userRoutes = require("./users");
+const projectRoutes = require("./projects");
+const tasksRoutes = require("./tasks");
 
-// Book routes
 router.use("/users", userRoutes);
+router.use("/projects", projectRoutes);
+router.use("/tasks", tasksRoutes);
 
 module.exports = router;
