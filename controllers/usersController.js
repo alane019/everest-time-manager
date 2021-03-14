449209721ff63daa458bc51bb888584afc1f7058
Pavel Darii <paveldarii@yahoo.com>
2021-03-14 17:11:24 -0400

    made little change in the methods for the userController

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -2,28 +2,28 @@ const db = require("../models");
 
 // Defining methods for the booksController
 module.exports = {
-  findAll: function (req, res) {
+  findAllUsers: function (req, res) {
     db.User.find(req.query)
       .sort({ date: -1 })
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
   },
-  findById: function (req, res) {
+  findUserById: function (req, res) {
     db.User.findById(req.params.id)
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
   },
-  create: function (req, res) {
+  createUser: function (req, res) {
     db.User.create(req.body)
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
   },
-  update: function (req, res) {
+  updateUser: function (req, res) {
     db.User.findOneAndUpdate({ _id: req.params.id }, req.body)
       .then((dbModel) => res.json(dbModel))
       .catch((err) => res.status(422).json(err));
   },
-  remove: function (req, res) {
+  removeUser: function (req, res) {
     db.User.findById({ _id: req.params.id })
       .then((dbModel) => dbModel.remove())
       .then((dbModel) => res.json(dbModel))
