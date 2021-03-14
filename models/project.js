d0a343669fc1ea38602fab6479cf192c91a43314
Pavel Darii <paveldarii@yahoo.com>
2021-03-14 17:03:04 -0400

    created a model for project collection

---
+++ models/project.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const projectSchema = new Schema({
+  userId: String,
+  name: String,
+  color: String,
+});
+const Project = mongoose.model("Project", projectSchema);
+
+module.exports = Project;
