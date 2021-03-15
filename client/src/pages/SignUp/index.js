3510584ddbc6dcd50746ce3353ebc7dc9c3c6bb6
Adam L <alane019@gmail.com>
2021-03-15 19:58:35 -0400

    update imports in signup and login pagess

--- client/src/pages/SignUp/index.js
+++ client/src/pages/SignUp/index.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import Form from "react-bootstrap/Form";
-import Button from "react-bootstrap/Button";
+//import Form from "react-bootstrap/Form";
+//import Button from "react-bootstrap/Button";
 import "./style.css";
 
 export default function Login() {
