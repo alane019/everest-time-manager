3fa3c9c166cf8cbd848c2c7b9393f4cac76a17f8
Eric Ober <ericscottober@gmail.com>
2021-03-16 18:47:26 -0400

    Added a main page for rendering

---
+++ client/src/pages/Main.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ProjectManager from "../components/ProjectManager";
+import Timer from "../components/Timer";
+
+function Main() {
+  return (
+    <div>
+      <ProjectManager></ProjectManager>
+      <Timer></Timer>
+    </div>
+  );
+}
+
+export default Main;
