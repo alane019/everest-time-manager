092efd22b24c6c6031124f97cddc85447e92103f
Pavel Darii <paveldarii@yahoo.com>
2021-03-17 15:54:36 -0400

    cleared the app and transformed it in a one page app

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,9 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import LogIn from "./pages/LogIn";
-import SignUp from "./pages/SignUp";
-import NoMatch from "./pages/NoMatch";
-import Main from "./pages/Main";
-import Nav from "./components/Nav";
-import ProjectManager from "./components/ProjectManager";
+
+import Login from "./components/LogIn";
 
 function App() {
-  return <LogIn />;
+  return <Login />;
 }
 
 export default App;
