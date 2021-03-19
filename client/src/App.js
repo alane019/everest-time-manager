e0e2739693df03456becde4e8832e841aa5dd8b5
Pavel Darii <paveldarii@yahoo.com>
2021-03-19 15:50:32 -0400

    added components that can be used for testing purposes

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Login from "./components/LogIn";
 import DropUpContainer from "./components/DropUpContainer";
 import ProjectManager from "./components/ProjectManager";
+import Main from "./components/Main";
 function App() {
   return <Login />;
 }
