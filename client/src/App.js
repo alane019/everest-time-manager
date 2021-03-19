6d532fe0f337dab0d9d51769da3a2df5e25127ef
Pavel Darii <paveldarii@yahoo.com>
2021-03-18 22:47:06 -0400

    imported ProjectManager component which can be used for development purposes

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Login from "./components/LogIn";
-import DropUpContainer from "./components/DropUpContainer"
+import DropUpContainer from "./components/DropUpContainer";
+import ProjectManager from "./components/ProjectManager";
 function App() {
   return <Login />;
 }
