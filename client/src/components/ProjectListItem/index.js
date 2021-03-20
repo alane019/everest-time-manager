906aaed4f3e761262b2a52b238267410b922b48a
Pavel Darii <paveldarii@yahoo.com>
2021-03-20 11:29:46 -0400

    created the basic structure of the task list item

--- client/src/components/ProjectListItem/index.js
+++ client/src/components/ProjectListItem/index.js
@@ -1,33 +1,68 @@
 import React from "react";
-import Main from "../Main";
+import { makeStyles } from "@material-ui/core/styles";
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemAvatar from "@material-ui/core/ListItemAvatar";
+import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
+import ListItemText from "@material-ui/core/ListItemText";
+import Avatar from "@material-ui/core/Avatar";
+import IconButton from "@material-ui/core/IconButton";
+import Grid from "@material-ui/core/Grid";
+import FolderIcon from "@material-ui/icons/Folder";
+import DeleteIcon from "@material-ui/icons/Delete";
 
-function ProjectListItem(props) {
-  
-   const displayTasks = () => {
-     // display a list item
-   }
-  
-  return (
-   
-   <div className="projectLi" onclick="displayTasks" key="1">   Project: Sport  </div>
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    maxWidth: 752,
+  },
+  demo: {
+    backgroundColor: theme.palette.background.paper,
+  },
+  title: {
+    margin: theme.spacing(4, 0, 2),
+  },
+}));
 
+function generate(element) {
+  return [0, 1, 2].map((value) =>
+    React.cloneElement(element, {
+      key: value,
+    })
   );
 }
 
-export default ProjectListItem;
+export default function InteractiveList() {
+  const classes = useStyles();
+  const [dense, setDense] = React.useState(false);
+  const [secondary, setSecondary] = React.useState(false);
 
-/*
-
-
-{
-    "_id": {
-        "$oid": "605567b9fe5c330296777431"
-    },
-  "userId": "Adam Lane",    
-  "name": "Jogging",
-  "startTime":{"$date":"2021-03-18T05:29:18.457Z"},"__v":0},
-  "createdAt":{"$date":"2021-03-18T05:28:43.209Z"},
-  "username":"Adam Lane","email":"alane019@gmail.com"
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={2}>
+        <Grid item xs={12} md={6}>
+          <div className={classes.demo}>
+            <List dense={dense}>
+              <ListItem>
+                <ListItemAvatar>
+                  <Avatar>
+                    <FolderIcon />
+                  </Avatar>
+                </ListItemAvatar>
+                <ListItemText
+                  primary="Single-line item"
+                  secondary={secondary ? "Secondary text" : null}
+                />
+                <ListItemSecondaryAction>
+                  <IconButton edge="end" aria-label="delete">
+                    <DeleteIcon />
+                  </IconButton>
+                </ListItemSecondaryAction>
+              </ListItem>
+            </List>
+          </div>
+        </Grid>
+      </Grid>
+    </div>
+  );
 }
-
-*/
