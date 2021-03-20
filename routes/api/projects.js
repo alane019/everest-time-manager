895e1cc8118fef819df0d06bf44deea1059296f2
Pavel Darii <paveldarii@yahoo.com>
2021-03-20 16:42:23 -0400

    added authorization as a middleware before accessing projects api routes

--- routes/api/projects.js
+++ routes/api/projects.js
@@ -1,15 +1,15 @@
 const router = require("express").Router();
 const projectsController = require("../../controllers/projectsController");
-
+const auth = require("../../config/middleware/auth");
 router
   .route("/:userId")
-  .get(projectsController.findAllProjects)
-  .post(projectsController.createProject);
+  .get(auth, projectsController.findAllProjects)
+  .post(auth, projectsController.createProject);
 
 router
   .route("/:userId/:projectId")
-  .get(projectsController.findProjectById)
-  .put(projectsController.updateProject)
-  .delete(projectsController.removeProject);
+  .get(auth, projectsController.findProjectById)
+  .put(auth, projectsController.updateProject)
+  .delete(auth, projectsController.removeProject);
 
 module.exports = router;
