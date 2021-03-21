800994e3941cc8cf0f64c928faf3ef255d2faf62
Pavel Darii <paveldarii@yahoo.com>
2021-03-21 16:51:52 -0400

    create the timer component look that can start and stop

--- client/src/components/Timer/index.js
+++ client/src/components/Timer/index.js
@@ -1,7 +1,9 @@
 import React, { useState, useEffect } from "react";
 import "./style.css";
-
-
+import StopIcon from "@material-ui/icons/Stop";
+import PlayArrowIcon from "@material-ui/icons/PlayArrow";
+import Box from "@material-ui/core/Box";
+import Icon from "@material-ui/core/Icon";
 const Timer = () => {
   const [second, setSecond] = useState("00");
   const [minute, setMinute] = useState("00");
@@ -44,22 +46,42 @@ const Timer = () => {
 
   return (
     <div className="container-fluid">
-      <div className="time">
-        <span className="minute">{minute}</span>
-        <span>:</span>
-        <span className="second">{second}</span>
-      </div>
-      <div className="buttons">
-        <button onClick={() => setIsActive(!isActive)} className="start">
-          {isActive ? "◼" : "▶"}
-        </button>
-        {/* <button onClick={stopTimer} className="reset">
-          Reset
-        </button> */}
-      </div>
+      <Box display="flex" flexDirection="row">
+        <Box
+          style={{
+            paddingRight: "0px",
+            borderRadius: "15px 0px 0px 15px",
+            background: "#c9d1c8de",
+          }}
+          p={1}
+          onClick={() => setIsActive(!isActive)}
+          className="start"
+        >
+          {isActive ? <StopIcon /> : <PlayArrowIcon />}
+        </Box>
+        <Box
+          p={1}
+          style={{
+            paddingLeft: "0px",
+            paddingTop: "7px",
+            borderRadius: "0px 15px 15px 0px",
+            background: "#c9d1c8de",
+          }}
+        >
+          {isActive ? (
+            <div className="time">
+              <span className="minute">{minute}</span>
+              <span>:</span>
+              <span className="second">{second}</span>
+            </div>
+          ) : (
+            <span style={{ fontSize: "16px" }}>start</span>
+          )}
+        </Box>
+      </Box>
+      <div className="buttons"></div>
     </div>
   );
 };
-
 
 export default Timer;
