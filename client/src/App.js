eeade9cdb8ff6bc9fd911bbe5e5138011ab955ad
Pavel Darii <paveldarii@yahoo.com>
2021-03-21 01:45:45 -0400

    added components for dev purposes

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,11 @@ import React from "react";
 import Login from "./components/LogIn";
 import DropUpContainer from "./components/DropUpContainer";
 import ProjectManager from "./components/ProjectManager";
-import Main from "./components/Main";
 import TaskManager from "./components/TaskManager";
 import ProjectListItem from "./components/ProjectListItem";
+import Main from "./components/Main";
 function App() {
-  return <Login />;
+  return <Main />;
 }
 
 export default App;
