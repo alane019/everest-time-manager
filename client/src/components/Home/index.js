91897e896e48a7cfbc671c16b13bf7d2b2a86e8b
Pavel Darii <paveldarii@yahoo.com>
2021-03-24 15:21:04 -0400

    created a function that will check weather the are there any active tasks and if yes then display respective layout on the home page

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import DropUpContainer from "../DropUpContainer";
+import ActiveTask from "../ActiveTask";
 function Home() {
-  return (
-    <div>
-      <h1>Hello</h1>
-      <DropUpContainer />
-    </div>
-  );
+  const [activeTaskStatus, setActiveStatus] = useState(true);
+  const displayHome = (active) => {
+    if (active) {
+      return (
+        <div>
+          <ActiveTask />
+          <DropUpContainer height="83vh" />
+        </div>
+      );
+    } else {
+      return (
+        <div>
+          <h1>History list</h1>
+          <DropUpContainer height="94vh" />
+        </div>
+      );
+    }
+  };
+  return displayHome(activeTaskStatus);
 }
 
 export default Home;
