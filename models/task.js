24b56595f418e6ea0f3c658ee35eb334c5b2dd83
Pavel Darii <paveldarii@yahoo.com>
2021-03-27 21:11:57 -0400

    removed color field

--- models/task.js
+++ models/task.js
@@ -3,7 +3,6 @@ const Schema = mongoose.Schema;
 
 const taskSchema = new Schema({
   name: String,
-  color: String,
   user: { type: Schema.Types.ObjectId, ref: "User" },
   project: { type: Schema.Types.ObjectId, ref: "Project" },
 });
