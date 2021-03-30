82a87c3944c290bcc3faef7abfd40d7c04475a38
Adam L <alane019@gmail.com>
2021-03-29 20:45:46 -0400

    additional updates for history list

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,12 @@
 import React from "react";
 import Login from "./components/LogIn";
+import ActionHistory from "./components/ActionHistory"
 
 function App() {
   // if (localStorage.getItem("token")) {
   //   return <Main></Main>;
   // }
-  return <Login />;
+  return <ActionHistory />;
 }
 
 export default App;
