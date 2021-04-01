a2605479519f50340e9a3f7381c13bed104ea40d
Pavel Darii <paveldarii@yahoo.com>
2021-04-01 14:37:04 -0400

    remodeled AddTaskForm

--- client/src/components/AddTaskForm/index.js
+++ client/src/components/AddTaskForm/index.js
@@ -5,67 +5,31 @@ import InputBase from "@material-ui/core/InputBase";
 import { fade, makeStyles } from "@material-ui/core/styles";
 import AddIcon from "@material-ui/icons/Add";
 import IconButton from "@material-ui/core/IconButton";
-import { Icon } from "@material-ui/core";
 import ListItemSecondaryAction from "@material-ui/core/ListItemSecondaryAction";
-
-const useStyles = makeStyles((theme) => ({
-  searchIcon: {
-    padding: theme.spacing(0, 2),
-    height: "100%",
-    position: "absolute",
-    pointerEvents: "none",
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  inputRoot: {
-    color: "inherit",
-  },
-  inputInput: {
-    padding: theme.spacing(1, 1, 1, 0),
-    // vertical padding + font size from searchIcon
-    paddingLeft: "5px",
-    paddingRight: `calc(1em + ${theme.spacing(4)}px)`,
-    transition: theme.transitions.create("width"),
-    width: "100%",
-    [theme.breakpoints.up("sm")]: {
-      width: "20ch",
-      "&:focus": {
-        width: "30ch",
-      },
-    },
-  },
-}));
 
 export default function AddForm(props) {
   const [inputText, setInputText] = useState("");
-  const classes = useStyles();
-  console.log(inputText);
+  const [inputPlaceholder, setInputPlaceholder] = useState("Create Task ...");
 
   return (
     <AppBar position="static" style={{ borderRadius: "15px" }}>
       <Toolbar>
         <InputBase
           onChange={(e) => setInputText(e.target.value)}
-          placeholder="Create Task ..."
-          classes={{
-            root: classes.inputRoot,
-            input: classes.inputInput,
-          }}
+          placeholder={inputPlaceholder}
           inputProps={{ "aria-label": "search" }}
+          style={{ color: "white" }}
         />
 
         <ListItemSecondaryAction>
-          <IconButton style={{ color: "white" }}>
-            <AddIcon
-              onClick={() => {
-                if (inputText === "") {
-                  return;
-                }
-                props.addTask(props.projectId, inputText);
-                setInputText("");
-              }}
-            />
+          <IconButton
+            style={{ color: "white" }}
+            onClick={(e) => {
+              setInputPlaceholder("Create Task ...");
+              props.addTask(props.projectId, inputText);
+            }}
+          >
+            <AddIcon />
           </IconButton>
         </ListItemSecondaryAction>
       </Toolbar>
