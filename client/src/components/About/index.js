630c2b2f86d8c466d2e0799f7e248c056ff51d7e
Matt Trader <matthewtrader@sbcglobal.net>
2021-04-03 11:29:44 -0400

    updated About with CSS

--- client/src/components/About/index.js
+++ client/src/components/About/index.js
@@ -1,27 +1,36 @@
 import React from "react";
+import "./style.css";
+
+return (
+  <div className="card-container" >
+     {
+       
+     }
+   </div>
+ );
 
 function About() {
   return (
     <div>
       <p>
-        The Productivity Tracker App is about achieving your desired goals
-        through customizable tasks and activities that you track with ease
-        daily.
+        The Everest App is about achieving your desired goals
+        through customizable tasks and activities that you track with ease on a daily basis.
       </p>
+
       <p>
-        Establishing daily goals in a visible format is scientifically proven to
+        Establishing daily goals in a tangible format is scientifically proven to
         provide an enhanced focus that leads to increased productivity (and
         likely increased earnings!). In 1979, researchers at "The Harvard MBA
         Business School" conducted a study highlighting the importance of goal
-        setting. Furthermore, writing down those desired goals is essential to
-        the likelihood of achieving your goals.
+        setting. Furthermore, documenting those desired goals is essential to
+        the likelihood of achieving your goals. 
+        
         https://www.forbes.com/sites/annabelacton/2017/11/03/how-to-set-goals-and-why-you-should-do-it/?sh=199f3e97162d
       </p>
       <p>
-        The Productivity Tracker App is an amazing and convenient tool to stay
+        The Everest App is an amazingly simple and convenient tool to stay
         on-task, get more done, and enjoy the personal rewards of
-        accomplishment! Sign-up now and start getting more done in your life
-        today!
+        accomplishment! Start getting more done in your life today!
       </p>
     </div>
   );
