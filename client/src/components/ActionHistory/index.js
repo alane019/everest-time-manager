940f832246c91e5c1d80051a391528a92ee90c51
Pavel Darii <paveldarii@yahoo.com>
2021-04-04 23:57:02 -0400

    implemented the title of the history list container with number of actions

--- client/src/components/ActionHistory/index.js
+++ client/src/components/ActionHistory/index.js
@@ -2,8 +2,7 @@ import API from "../../utils/API";
 import "./style.css";
 import React, { useState, useEffect } from "react";
 import ActionHistoryList from "../ActionHistoryList";
-import HistoryIcon from "../../assets/OutlineHistoryBlack24dp.png";
-const moment = require('moment');
+const moment = require("moment");
 
 function ActionHistory(props) {
   const [actionData, setActions] = useState([]);
@@ -16,28 +15,52 @@ function ActionHistory(props) {
       .then((res) => {
         setActions(res.data);
       })
-  
+
       .catch((error) => console.log(error));
   }
-      
-
+  function getActionAmount() {
+    if (actionData.length) {
+      return actionData.length;
+    }
+    return 0;
+  }
 
   return (
-    <div  id="history-list-container" className="card-container">
-     {
-      actionData.sort((a, b) => b.startTime - a.startTime).reverse()
-      .map((action) => (
-        <ActionHistoryList
-          key={action._id}
-          taskName={action.task.name}
-          projectName={action.project.name}
-          projectColor={action.project.color}
-          startTime={moment(action.startTime).format("dddd, MMMM DD YYYY, h:mm a")}
-          endTime={moment(action.endTime).format("dddd, MMMM DD YYYY, h:mm a")}
-          duration={(moment.utc(action.duration*1000).format('HH:mm:ss'))}
-        />
-      ))}
-  </div>
+    <div
+      id="history-list-container"
+      className="card-container"
+      style={{ marginTop: props.containerStyle.historyListHeight }}
+    >
+      <h3 style={{ textAlign: "center" }}>
+        <i>
+          <strong>
+            Total of{" "}
+            <span style={{ fontSize: "100%", color: "tomato" }}>
+              {getActionAmount()}
+            </span>{" "}
+            actions made
+          </strong>
+        </i>
+      </h3>
+      {actionData
+        .sort((a, b) => b.startTime - a.startTime)
+        .reverse()
+        .map((action) => (
+          <ActionHistoryList
+            key={action._id}
+            taskName={action.task.name}
+            projectName={action.project.name}
+            projectColor={action.project.color}
+            startTime={moment(action.startTime).format(
+              "dddd, MMMM DD YYYY, h:mm a"
+            )}
+            endTime={moment(action.endTime).format(
+              "dddd, MMMM DD YYYY, h:mm a"
+            )}
+            duration={moment.utc(action.duration * 1000).format("HH:mm:ss")}
+          />
+        ))}
+    </div>
   );
 }
 
